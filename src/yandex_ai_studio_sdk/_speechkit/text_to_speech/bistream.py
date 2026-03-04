9a5c5457e4c0aed7ba76b5b80c0b5a17dfcaf8de
Vladimir Lipkin <lipkin@yandex-team.ru>
2026-03-04 18:32:59 +0300

    Isort

--- src/yandex_ai_studio_sdk/_speechkit/text_to_speech/bistream.py
+++ src/yandex_ai_studio_sdk/_speechkit/text_to_speech/bistream.py
@@ -11,7 +11,6 @@ from yandex.cloud.ai.tts.v3.tts_pb2 import (
     SynthesisOptions
 )
 from yandex.cloud.ai.tts.v3.tts_service_pb2_grpc import SynthesizerStub
-
 from yandex_ai_studio_sdk._client import AsyncCloudClient
 from yandex_ai_studio_sdk._speechkit.enums import AudioFormat
 from yandex_ai_studio_sdk._types.proto import SDKType
